f9141bfaf2ffa831f7bef614305f9017622fe6f1
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-04-26 16:51:23 +0200

    fix manifest description max size

---
+++ index.js
@@ -0,0 +1,28 @@
+  
+  let iframe = document.createElement("iframe")
+  iframe.id = "targetPage";
+  
+  iframe.height = "100%";
+  iframe.width = "100%";
+  iframe.src = new URL(window.location.href).searchParams.get('u')||"https://hydra.ojack.xyz/";
+  
+  document.body.appendChild(iframe);
+
+  
+  wrapper = document.createElement("a");
+  wrapper.href = "";
+  wrapper.style = "position:fixed; z-index:10000; bottom:10px; right:10px; background:rgb(0,0,0,0.5); border-radius:3px; padding:5px;"
+  
+  image=document.createElement("img");
+  image.src= "images/icon-48.png";
+  image.style="height:20px";
+  logoText = document.createElement("div");
+  logoText.innerHTML = "get scraw.link extension";
+  logoText.style="display: inline; color: #00ff00;font-family: monospace;font-size: 14px;font-weight: bold; margin: 5px;";
+
+  wrapper.appendChild(image);
+  wrapper.appendChild(logoText);
+  
+  document.body.appendChild(wrapper);
+  
+  
