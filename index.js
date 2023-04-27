181c254a5e7b72f38bd6492bdbe525876e4fc278
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-04-27 17:50:13 +0200

    add Telegram group link

--- index.js
+++ index.js
@@ -8,12 +8,15 @@
   
   document.body.appendChild(iframe);
   
-  wrapper = document.createElement("a");
-  wrapper.href = "build/scrawlink-extension.zip";
+  wrapper = document.createElement("div");
+  wrapper.className += "instructions";
   wrapper.style = "position:fixed; z-index:10000; bottom:10px; right:10px; background:rgb(0,0,0,0.5); border-radius:3px; padding:5px;"
   
+  link = document.createElement("a");
+  link.href = "build/scrawlink-extension.zip";
+
   
-  wrapper.onclick = function(){
+  link.onclick = function(){
     console.log('clicked');
     window.open('https://www.scraw.link/instructions.html');
   }
@@ -22,12 +25,20 @@
   image.src= "images/icon-48.png";
   image.style="height:20px";
   logoText = document.createElement("div");
-  logoText.innerHTML = "get scraw.link extension";
-  logoText.style="display: inline; color: #00ff00;font-family: monospace;font-size: 14px;font-weight: bold; margin: 5px;";
+  logoText.innerHTML = "Get scraw.link extension";
+  logoText.style="display: inline;  margin: 5px;";
 
-  wrapper.appendChild(image);
-  wrapper.appendChild(logoText);
+  telegram = document.createElement("div");
+  telegram.innerHTML = "Join <a href='https://t.me/+WC6bX1jGnhA5ZTk8'>Scraw.Link Telegram group</a>"
+
+
+  link.appendChild(image);
+  link.appendChild(logoText);
+
+  wrapper.appendChild(link);
+  wrapper.appendChild(telegram);
   
   document.body.appendChild(wrapper);
+  document.body.appendChild(wrapper2);
   
   
