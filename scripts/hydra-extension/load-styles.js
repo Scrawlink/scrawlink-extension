354478f6cc89b81f010b13636eff4a410f9e5679
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-09-13 21:18:32 +0200

    fix styles url

--- scripts/hydra-extension/load-styles.js
+++ scripts/hydra-extension/load-styles.js
@@ -1,7 +1,7 @@
 const loadStyles = function(){
   let style= document.createElement("link")
   style.setAttribute("rel", "stylesheet")
-  style.setAttribute("href", "https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/scripts/fork-awesome.min.css" )
+  style.setAttribute("href", "https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/style.css" )
   style.setAttribute("crossorigin","anonymous")
   document.head.appendChild(style)
 }
