d39ec02a3d967ebad1b911bfe3582d7e63c53a03
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-09-13 21:32:06 +0200

    fix load script

--- scripts/hydra-extension/hydra-scrawlink.js
+++ scripts/hydra-extension/hydra-scrawlink.js
@@ -1,7 +1,9 @@
-loadScrawlink = function(){
+async function loadScrawlink(){
   if (! document.getElementById("scrawlink")) {
     await loadScript("https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/scripts/qrcode.min.js")
     await loadScript("https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/scripts/content.js")
     await loadScript("https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/scripts/hydra-extension/load-styles.js")
   }
 }
+
+loadScrawlink()
