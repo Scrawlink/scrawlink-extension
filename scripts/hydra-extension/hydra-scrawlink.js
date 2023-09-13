997364cee1014f1ec6f41b035532f79a80241882
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-09-13 21:28:35 +0200

    add hydra extension load script

---
+++ scripts/hydra-extension/hydra-scrawlink.js
@@ -0,0 +1,7 @@
+loadScrawlink = function(){
+  if (! document.getElementById("scrawlink")) {
+    await loadScript("https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/scripts/qrcode.min.js")
+    await loadScript("https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/scripts/content.js")
+    await loadScript("https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/scripts/hydra-extension/load-styles.js")
+  }
+}
