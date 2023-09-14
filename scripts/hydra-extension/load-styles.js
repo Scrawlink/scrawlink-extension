642b56c343f69d574c40f0715a25f4def2bb3953
Ámbar Tenorio Fornés <ambar@atenor.io>
2023-09-14 14:23:26 +0200

    add fork awesome stylesheet for hydra extension

--- scripts/hydra-extension/load-styles.js
+++ scripts/hydra-extension/load-styles.js
@@ -4,6 +4,13 @@ const loadStyles = function(){
   style.setAttribute("href", "https://cdn.jsdelivr.net/gh/Scrawlink/scrawlink-extension@latest/style.css" )
   style.setAttribute("crossorigin","anonymous")
   document.head.appendChild(style)
+
+  let fa = document.createElement("link")
+  fa.setAttribute("rel", "stylesheet")
+  fa.setAttribute("href", "https://cdn.jsdelivr.net/npm/fork-awesome@1.2.0/css/fork-awesome.min.css" )
+  fa.setAttribute("integrity","sha256-XoaMnoYC5TH6/+ihMEnospgm0J1PM/nioxbOUdnM8HY=")
+  fa.setAttribute("crossorigin","anonymous")
+  document.head.appendChild(fa)
 }
 
 loadStyles()
